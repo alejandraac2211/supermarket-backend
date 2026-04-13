c859efd6b7f8c4660f9d993564a703157d7c7eab
Breayanrc11 <83018696+Breayanrc11@users.noreply.github.com>
2026-04-12 20:01:17 -0500

    Ajuste codigo

--- app.js
+++ app.js
@@ -9,24 +9,19 @@ const saleRoutes = require('./routes/saleRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
-
-//Primero middlewares
 app.use(cors());
 app.use(express.json());
 
-//Las rutas
 app.use('/api/products', productRoutes);
 app.use('/api/providers', providerRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/sales', saleRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-//Ruta de prueba
 app.get('/api', (req, res) => {
   res.json({ message: 'API Funcionando' });
 });
 
-//Conexión y servidor
 db.sequelize.sync().then(() => {
   app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
